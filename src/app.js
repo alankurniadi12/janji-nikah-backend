6eed3de6900c044fa4a0269162f2792d71646ee6
Alan Kurniadi <alankurniadi53@gmail.com>
2026-07-28 20:30:56 +0700

    fix(backend): allow uploaded assets cross origin

--- src/app.js
+++ src/app.js
@@ -17,7 +17,13 @@ export function createApp() {
 
   app.set("trust proxy", 1);
 
-  app.use(helmet());
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: {
+        policy: "cross-origin"
+      }
+    })
+  );
   app.use(
     cors({
       origin(origin, callback) {
